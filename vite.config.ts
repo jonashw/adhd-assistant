be7558c0955968bbe9d07465437e3a22ed7aa211
Jon Wilson <Jon.Wilson@treasurer.ca.gov>
2024-05-27 22:55:45 -0700

    no certs on build; certs for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from "fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({command}) => {
+  const cfg = { plugins: [react()] };
+  return command === "build" 
+  ? cfg
+  : {
+    ...cfg,
+    server: {
+      https: {
+        key: fs.readFileSync('./.cert/key.pem'),
+        cert: fs.readFileSync('./.cert/cert.pem'),
+      }
+    }
+  }
 });
